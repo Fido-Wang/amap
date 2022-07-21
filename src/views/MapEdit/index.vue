06dbc1ac3440c96472a035b33ebcf3eb1350c9a1
wangqi <742927821@qq.com>
2022-07-21 22:37:48 +0800

    周四晚上

--- src/views/MapEdit/index.vue
+++ src/views/MapEdit/index.vue
@@ -4,9 +4,11 @@
 <!--    <button @click="draw('marker')">打点</button>-->
     <button @click="drawMarker">打点</button>
     <button @click="end">结束</button>
+    <button @click="editPolyline">修改线段</button>
 <!--    <button @click="continueDrawLine">continue</button>-->
     <div id="container">
       <div class="left-drawer" :style="{ display: isShowDrawer }">
+        <h3>修改标记点</h3>
         <el-form ref="form" :model="form" label-width="80px">
           <el-form-item label="标签名称">
             <el-input v-model="form.markerName"></el-input>
@@ -25,6 +27,45 @@
               <el-option label="事件点" value="4"></el-option>
             </el-select>
           </el-form-item>
+          <el-form-item>
+
+            <el-button type="primary" @click="closeDrawer">取消</el-button>
+            <el-button type="primary" @click="savePointInfo">保存</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+
+
+      <div class="left-drawer" :style="{ display: isShowLineEditDrawer }">
+        <h3>修改线段</h3>
+        <el-form ref="form" :model="lineForm" label-width="100px">
+          <el-form-item label="线段类型">
+            <el-select v-model="lineForm.type" placeholder="请选择">
+              <el-option label="里程控制点" value="1"></el-option>
+              <el-option label="构筑物" value="2"></el-option>
+              <el-option label="标段分割点" value="3"></el-option>
+              <el-option label="事件点" value="4"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="施工长度/米">
+            <el-input v-model="lineForm.length"></el-input>
+          </el-form-item>
+          <el-form-item label="当前状态">
+            <el-input v-model="lineForm.status" placeholder="例：129.12345"></el-input>
+          </el-form-item>
+          <el-form-item label="沟槽深度">
+            <el-input v-model="lineForm.depth" placeholder="例：129.12345"></el-input>
+          </el-form-item>
+          <el-form-item label="沟槽宽度">
+            <el-input v-model="lineForm.width" placeholder="例：129.12345"></el-input>
+          </el-form-item>
+          <el-form-item label="垫层厚度">
+            <el-input v-model="lineForm.thickness" placeholder="例：129.12345"></el-input>
+          </el-form-item>
+          <el-form-item label="垫层材质">
+            <el-input v-model="lineForm.material" placeholder="例：129.12345"></el-input>
+          </el-form-item>
+
           <el-form-item>
 
             <el-button type="primary" @click="closeDrawer">取消</el-button>
@@ -69,14 +110,29 @@ export default {
       isDrawPolyline: false, // 当前是否为画线
       curMarkerPosition: {}, // 当前打点的marker的位置
       isShowDrawer: 'none', // 是否展示左侧drawer
+      // 当前marker的info
       form: {
         markerName: '',
         markerLng: '',
         markerLat: '',
         markerType: ''
       },
+      // 当前polyline的info
+      lineForm: {
+        type: '', // 线段类型
+        length: '', // 施工长度（米）
+        status: '', // 当前状态
+        depth: '', // 沟槽深度
+        width: '', // 沟槽宽度
+        thickness: '', // 垫层厚度
+        material: '', // 垫层材质
+        path: [], // 线段的path数组
+      },
       curEditPoint: [],
       curEditIndex: [], // 当前正在编辑的marker的点的inidex
+      curPolylinePath: [], // 当前正在编辑的线段的path
+      curPolylineObj: {}, // 当前正在编辑的线段的实例对象
+      isShowLineEditDrawer: 'none', // 是否展示编辑线段的drawer
     }
   },
   mounted() {
@@ -225,6 +281,11 @@ export default {
 
         })
 
+        // 获取覆盖物
+        // that.map.getAllOverlays((e)=> {
+        //   console.log('获取覆盖物', e)
+        // })
+
         // that.map.event.addListener("polyline",'click',function(e){
         //   console.log('eeeeeee', e)  //设置组成该折线的节点数组
         // })
@@ -284,6 +345,7 @@ export default {
     // marker的drawer取消按钮
     closeDrawer() {
       this.isShowDrawer = "none"
+      this.isShowLineEditDrawer = "none"
     },
     // 保存marker的drawer的按钮
     savePointInfo() {
@@ -295,7 +357,28 @@ export default {
       this.allMarkerObject[this.curEditIndex]._position.lng = Number(this.form.markerLng)
       this.allMarkerObject[this.curEditIndex]._position.lat = Number(this.form.markerLat)
       this.isShowDrawer = 'none'
+      this.isShowLineEditDrawer = 'none'
       // console.log('修改之后的 allPointArray', this.allPointArray)
+    },
+
+    // 修改线段 获取地图上所有的覆盖物 过滤出所有的线段 给每个线段绑定点击事件
+    editPolyline() {
+      let that = this
+      console.log(that.map.getAllOverlays())
+      let allCoverObj = that.map.getAllOverlays()
+      console.log(allCoverObj)
+      let polylineCoverObj = allCoverObj.filter(item=> {
+        return item.type == "AMap.Overlay"
+      })
+      console.log(polylineCoverObj)
+      polylineCoverObj.forEach(item=> {
+        item.on('click', (e)=> {
+          console.log('click line', e, e.target._opts.path) // 点击的这条线段的所有的点的集合
+          that.curPolylineObj = e // 保存当前正在编辑的线段的实例
+          that.curPolylinePath = e.target._opts.path // 保存当前正在编辑的线段的关键点的经纬度集合
+          that.isShowLineEditDrawer = 'block'
+        })
+      })
     },
     handleClose() { }
   }
